54b11271b1b879e33b31ba7672e206d7f0904ada
Fernando Motta <fernandosantosmotta@gmail.com>
2022-10-11 14:26:41 -0300

    first commit

---
+++ Crypto Coin/Model/CoinData.swift
@@ -0,0 +1,8 @@
+//
+//  CoinData.swift
+//  Crypto Coin
+//
+//  Created by Fernando dos Santos Motta on 23/07/22.
+//
+
+import Foundation
---
+++ Crypto Coin/Model/CoinManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoinManager.swift
+//  Crypto Coin
+//
+//  Created by Fernando dos Santos Motta on 23/07/22.
+//
+
+import Foundation
